e02dea3e7560fceb27c2b195e481035fe1ea2210
novak.mateusz94@gmail.com <novak.mateusz94@gmail.com>
2021-08-05 18:00:54 +0200

    Receiving and sending back message from clinet

--- server.py
+++ server.py
@@ -5,5 +5,20 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Using TCP
 address = ('localhost', 8000)
 server_socket.bind(address)
 server_socket.listen()
-connection, client_address = server_socket.accept()
-print(f'Connection: {connection}, address: {client_address}')
+try:
+    connection, client_address = server_socket.accept()
+    print(f'Address: {client_address}')
+
+    buffer = connection.recv(2)  # Setting buffer's low capacity
+    print(f"Data received: {buffer}")
+
+    while buffer[-1:] != b'\n':
+        data = connection.recv(2)
+        print(f"Data received: {data}")
+        buffer += data
+
+    print(f"Add data: {buffer}")
+    connection.sendall(buffer)
+
+finally:
+    server_socket.close()
